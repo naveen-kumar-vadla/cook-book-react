1bfa0286a42fe1903e29717433c87be4e9e32b19
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-15 16:07:04 +0530

    Added router with HomePage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './css/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './components/HomePage.js';
 
-import HomePage from './components/HomePage.js'
-
-const App = () => <HomePage />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
