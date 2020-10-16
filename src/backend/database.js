50fd9ef757c95531389b5d24ddb2fd267dc464e8
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-16 08:56:34 +0530

    extracted database methods into itw own file

---
+++ src/backend/database.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const fetchRecipies = () => require('./data.json');
+const storeRecipies = recipies =>
+  fs.writeFileSync('./data.json', JSON.stringify(recipies));
+
+const fetchUsers = () => require('./users.json');
+const storeUsers = users =>
+  fs.writeFileSync('./users.json', JSON.stringify(users));
+
+const fetchRecipe = id => fetchRecipies().find(recipe => recipe.id === id);
+
+const fetchUser = (fieldToMatch, value) => {
+  const users = fetchUsers();
+  const user = users.find(user => user[fieldToMatch] === value);
+  return user ? user : {};
+};
+
+const saveUser = ({ name, login: username, avatar_url: imageUrl }) => {
+  const id = new Date().getTime() * Math.round(Math.random() + 1);
+  const user = { username, name, imageUrl, id };
+  const users = fetchUsers();
+  users.push(user);
+  storeUsers(users);
+  return user;
+};
+
+module.exports = {
+  fetchRecipies,
+  fetchUser,
+  fetchRecipe,
+  fetchUsers,
+  saveUser,
+};
--- src/backend/handlers.js
+++ src/backend/handlers.js
@@ -1,58 +1,41 @@
-const data = require('./data.json');
-const users = require('./users.json');
+const database = require('./database');
 
 const { HOMEPAGE_URL } = require('./config');
-
-const getUser = (fieldToMatch, value) => {
-  const user = users.find(user => user[fieldToMatch] === value);
-  return user ? user : {};
-};
 
 const logger = (req, res, next) => {
   console.log(req.method, req.url);
   next();
 };
 
-const serveRecipies = (req, res) => res.json(data);
+const serveRecipies = (req, res) => res.json(database.fetchRecipies());
 
 const serveUser = (req, res) => {
   const userId = Number(req.cookies.userId);
-  const user = getUser('id', userId);
+  const user = database.fetchUser('id', userId);
   res.json(user);
 };
 
 const serveRecipe = (req, res) => {
   const id = Number(req.params.id);
-  const recipe = data.find(recipe => recipe.id === id);
+  const recipe = database.fetchRecipe(id);
   res.json(recipe);
 };
 
-const authorizeUser = function (request, response) {
-  const auth = request.app.locals.auth;
-  response.redirect(auth.getAuthorizeUrl());
+const authorizeUser = (req, res) => {
+  const auth = req.app.locals.auth;
+  res.redirect(auth.getAuthorizeUrl());
 };
 
-const loginUser = async function (request, response) {
+const loginUser = async (req, res) => {
   console.log('HOMEPAGE_URL', HOMEPAGE_URL);
-  const { code, error } = request.query;
-  if (error) {
-    return response.redirect(HOMEPAGE_URL);
-  }
-  const { auth } = request.app.locals;
+  const { code, error } = req.query;
+  if (error) return res.redirect(HOMEPAGE_URL);
+  const { auth } = req.app.locals;
   const details = await auth.fetchUserDetails(code);
-  let user = getUser('username', details.login);
-  if (!user.id) {
-    const id = new Date().getTime() * Math.round(Math.random() + 1);
-    user = {
-      username: details.login,
-      name: details.name,
-      imageUrl: details.avatar_url,
-      id,
-    };
-    users.push(user);
-  }
-  response.cookie('userId', user.id);
-  return response.redirect(HOMEPAGE_URL);
+  let user = database.fetchUser('username', details.login);
+  if (!user.id) user = database.saveUser(details);
+  res.cookie('userId', user.id);
+  return res.redirect(HOMEPAGE_URL);
 };
 
 module.exports = {
