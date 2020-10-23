71a10152573c58eb0d6168241c8d6d037994075c
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-23 11:58:39 +0530

    moved backend out of it

--- src/backend/config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-process-env */
-
-module.exports = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  PORT: process.env.PORT,
-  COOKIE_SECRET: process.env.COOKIE_SECRET,
-  HOMEPAGE_URL: process.env.HOMEPAGE_URL,
-};
--- src/backend/server.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const Auth = require('./auth');
-
-const { CLIENT_ID, CLIENT_SECRET, PORT } = require('./config');
-
-const app = require('./routes');
-
-app.locals.auth = new Auth(CLIENT_ID, CLIENT_SECRET, axios);
-app.listen(PORT, () => process.stdout.write(`listening on ${PORT}\n`));
